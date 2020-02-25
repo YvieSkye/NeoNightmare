3ddd86b660126637ed195e745bcfbd342c7b6e6c
Vijay <vj.rajan@yoobeecolleges.com>
2020-02-25 15:11:44 +1300

    pushing the fixed spawning & character follow

--- NeoNightmare/Assets/EnemySpawn.cs
+++ NeoNightmare/Assets/EnemySpawn.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject [] enemies;
+    public GameObject[] enemies;
     public Vector3 spawnvalues;
     public float spawnWait;
     public float spawnMostWait;
     public float spawnLeastWait;
+    public int startWait;
     public bool stop;
+
     int randEnemy;
 
     void Start()
     {
         StartCoroutine(waitSpawner());
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+
     }
 
     IEnumerator waitSpawner()
     {
-        yield return new WaitForSeconds(spawnWait);
+        yield return new WaitForSeconds(startWait);
 
         while (!stop)
         {
@@ -33,10 +37,11 @@ public class EnemySpawn : MonoBehaviour
 
             Vector3 spawnPosition = new Vector3(Random.Range(-spawnvalues.x, spawnvalues.x), 1, (Random.Range(-spawnvalues.z, spawnvalues.z)));
 
-            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0 , 0, 0), gameObject.transform.rotation);
 
             yield return new WaitForSeconds(spawnWait);
         }
     }
 }
+
 
---
+++ NeoNightmare/Assets/FollowTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class FollowTest : MonoBehaviour {
+    public GameObject player;
+    public float upforce;
+    public float forwardforce;
+    public Rigidbody rb;
+    public float delay;
+   
+   
+
+
+    public bool canstep;
+
+	
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        canstep = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+	}
+	
+	
+	void Update () {
+
+        if (canstep)
+        {
+            StartCoroutine(Step());
+        }
+		
+	}
+
+    public IEnumerator Step()
+    {
+        canstep = false;
+        transform.LookAt(player.transform);
+        rb.AddForce(Vector3.up * upforce);
+        rb.AddRelativeForce(Vector3.forward * forwardforce);
+        yield return new WaitForSeconds(delay);
+        canstep = true;
+
+       
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+                 
+            Destroy(this.gameObject);
+        }
+    }
+}
