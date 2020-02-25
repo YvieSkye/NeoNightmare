4f7dd29897f410e5f8d5937b80b3089e0f1df118
Yvie <yvearnaskye@gmail.com>
2020-02-25 13:52:13 +1300

    Unity Project

---
+++ NeoNightmare/Assets/EnemyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform Player;
+    public float speed = 2f;
+    private float minDistance = 0.2f;
+    private float range;
+    void Update()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+        range = Vector2.Distance(transform.position, Player.position);
+        if (range > minDistance)
+        {
+            Debug.Log(range);
+            Player = GameObject.FindWithTag("Player").transform;
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ NeoNightmare/Assets/EnemySpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject [] enemies;
+    public Vector3 spawnvalues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public bool stop;
+    int randEnemy;
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(spawnWait);
+
+        while (!stop)
+        {
+            randEnemy = Random.Range(0, 4);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnvalues.x, spawnvalues.x), 1, (Random.Range(-spawnvalues.z, spawnvalues.z)));
+
+            Instantiate(enemies[randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
+
