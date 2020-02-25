b4c0cf50860a6c99b2df97e07a904aa82c731435
YvieSkye <57970260+YvieSkye@users.noreply.github.com>
2020-02-25 13:05:57 +1300

    Add files via upload

---
+++ EnemyFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction*moveSpeed * Time.deltaTime));
+    }
+}
